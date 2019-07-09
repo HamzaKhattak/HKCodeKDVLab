56bcfef1c1c3dfa06e8b67dba2d9db68a0624f71
Hamza Khattak <hamzakhattak@trentu,ca>
2019-07-09 17:41:17 -0400

    Update edgefinder.py

--- edgefinder.py
+++ edgefinder.py
@@ -33,7 +33,7 @@ plt.show()
 plt.figure()
 plt.imshow(imsequence[-1])
 #%%
-#Check crop
+#Check crop and image edge detection
 def cropper(seq,x1,x2,y1,y2,singleimage=False):
     if singleimage:
         return seq[y1:y2, x1:x2]
@@ -44,84 +44,169 @@ imsequence2=cropper(imsequence[-2:],15,791,701,920)
 #subtract off background and invert
 plt.imshow(imsequence2[0])
 
+
+#%%
+#Check thresholding parameters
+
+#Import colors for plotting
+import matplotlib.colors as mcolors
+colors = [(0,1,0,c) for c in np.linspace(0,1,100)]
+cmapg = mcolors.LinearSegmentedColormap.from_list('mycmap', colors, N=5)
+
+
+def edgedetector(inimage,background,threshval,obsSize,cannysigma):
+    '''
+    This function finds the edges of a cropped image of a pipette and droplet
+    Image must be black on a brighter backdrop. Returns the result as a numpy
+    array type object
+    Arguments are:
+        inimage: The input image
+        background: A background image (or a 0 image)
+        threshval: Threshold to use for binary thresholding of image, may be negative
+        obSize: Maximum size of noise object to remove, choose smallest value that removes dust
+        cannysigma: Choose value of guassian blur for edge detection
+    '''
+    #Subtract background if needed and select image, droplet should be high so invert
+    imsub=background-inimage
+
+    
+    threshimage=imsub>threshval
+    #Fill holes
+    threshimage=morph2.binary_fill_holes(threshimage)
+    #Remove specs
+    threshimage=morph.remove_small_objects(threshimage,5)
+    #Find the edges
+    edgedetect=feature.canny(threshimage, sigma=.05)
+    return edgedetect
+
+edgedetect=edgedetector(imsequence[-1],background,-100,5,.05)
+#Plot to see
+plt.imshow(imsequence[-1],cmap=plt.cm.gray)
+plt.imshow(edgedetect, cmap=cmapg)
+plt.tight_layout()
+
+#%%
+#Test curve fit from edges
+edgearray=np.argwhere(edgedetect)
+
+def circle(x,a,b,r):
+    return np.sqrt(r**2-(x-a)**2)+b
+
+
+
+def slopeptline(x,m,x0,y0):
+    return m*(x-x0)+y0
+
+
+def splitlinefinder(locs,centerybuff):
+
+    #Which area to use for splitting line (ie how far up y)
+    splitlineavregion=np.max(locs[:,0])-centerybuff
+    splitline=np.mean(locs[:,1][locs[:,0]>splitlineavregion])
+    return splitline
+
+def edgeinfofinder(locs,left,pixelbuff,circfitguess,zweight):
+    '''
+    This function takes a numpy array of edge location xy values and returns
+    the location of the contact point as well as a fitted circle
+    Parameters:
+        locs: the input array
+        left: True for left side of droplet, false for right
+        pixelbuff: How many pixels in x to include for fit
+        circfitguess: Guess's for circle fit parameters, make sure to make negative for
+            right side. [xcenter,ycenter,radius]
+        zweight: anything below 1 gives extra weight to the zero
+        
+    '''
+    
+    if left==True:
+        contactloc=np.argmin(locs[:,1])
+    else:
+        contactloc=np.argmax(locs[:,1])
+    
+    contactx=locs[contactloc,1]
+    contacty=locs[contactloc,0]
+    
+    #Only bit different
+    if left==True:
+        conds=np.logical_and(locs[:,1]<contactx+pixelbuff,locs[:,0]>contacty)
+    else:
+        conds=np.logical_and(locs[:,1]>contactx-pixelbuff,locs[:,0]>contacty)
+        
+    trimDat=locs[conds]-[contacty,contactx]
+    
+    #Set up weighting
+    sigma = np.ones(len(trimDat[:,0]))
+    sigma[np.argmin(trimDat[:,0])] = zweight
+    #The fitter is annoyingly dependand on being close to the actual parameters values to get a good guess
+    popt, pcov = curve_fit(circle, trimDat[:,1], trimDat[:,0],p0=circfitguess, sigma=sigma,maxfev=5000)
+    def paramcirc(x):
+        return circle(x,*popt)
+    mcirc=derivative(paramcirc,0)
+    #Return angle in degrees
+    thet=np.arctan(mcirc)*180/np.pi
+    #Shift circle back
+    popt=popt+[contactx,contacty,0]
+    return [contactx,contacty,popt,thet,contactx]
+
+
+#Plot
+leftedgedatatest=edgeinfofinder(edgearray,True,30,[50,10,100],1)
+rightedgedatatest=edgeinfofinder(edgearray,False,15,[-50,10,100],1)
+
+x=np.linspace(leftedgedatatest[0],rightedgedatatest[0],100)
+plt.plot(x,circle(x,*leftedgedatatest[2]))
+plt.plot(x,circle(x,*rightedgedatatest[2]))
+plt.plot(edgearray[:,1],edgearray[:,0],'.',markersize=3)
+plt.axes().set_aspect('equal')
+
+#%%
+trimDatMaxr=trimDatright[:,1].max()
+maxPlotCirc=trimDatMaxr+50
+x=np.linspace(0,maxPlotCirc,100)
+plt.plot(locs[:,1],locs[:,0],'.',markersize=3)
+plt.plot(x+leftx,paramcirc(x)+lefty)
+
+p0=[50,10,100]
+plt.axvline(leftx)
+plt.axvline(rightx)
+plt.axes().set_aspect('equal')
+#%%
+30
+20
+
+#%%
+
+
+#%%
+
+plt.plot(leftdat[:,1],leftdat[:,0],'.',markersize=2)
+plt.plot(leftdat[minLoc,1],leftdat[minLoc,0],'o',markersize=3)
+#%%
+'''
+old
+oRem=morph.remove_small_objects(threshimage,100)
+hRem=morph.remove_small_holes(oRem,100)
+plt.imshow(hRem,cmap=plt.cm.gray)
+
+edgedetect=feature.canny(hRem, sigma=.05)
+plt.imshow(edgedetect, cmap=plt.cm.gray)
+locs=np.argwhere(edgedetect)
+'''
 #%%
 #Crop
 imsequence=cropper(imsequence,15,791,701,920)
-#%%
 background=cropper(background,15,791,701,920,True)
-#%%
-#Check thresholding parameters
-imsub=background-imsequence[0]
-
-threshimage=imsub>-25
-
-plt.imshow(threshimage,cmap=plt.cm.gray)
-#%%
-threshimage2=morph2.binary_fill_holes(threshimage)
-threshimage3=morph2.binary_closing(threshimage2,iterations=2)
-edgedetect=feature.canny(threshimage3, sigma=.05)
-
-locs=np.argwhere(edgedetect)
-
-import matplotlib.colors as mcolors
-colors = [(0,0,1,c) for c in np.linspace(0,1,100)]
-
-cmapblue = mcolors.LinearSegmentedColormap.from_list('mycmap', colors, N=5)
-
-plt.imshow(imsub,cmap=plt.cm.gray)
-plt.imshow(edgedetect, cmap=cmapblue)
-#%%
-
-oRem=morph.remove_small_objects(threshimage,100)
-hRem=morph.remove_small_holes(oRem,100)
-plt.imshow(hRem,cmap=plt.cm.gray)
-
-edgedetect=feature.canny(hRem, sigma=.05)
-plt.imshow(edgedetect, cmap=plt.cm.gray)
-locs=np.argwhere(edgedetect)
-
-#%%
-
-background=ndi.imread('background.png',flatten=True)
-im = ndi.imread('Images/moveseq103.png',flatten=True)
-
-imsub=background-im
-
-threshimage=imsub>10
-plt.imshow(threshimage,cmap=plt.cm.gray)
-
-
-oRem=morph.remove_small_objects(threshimage,100)
-hRem=morph.remove_small_holes(oRem,100)
-plt.imshow(hRem,cmap=plt.cm.gray)
-
-edgedetect=feature.canny(hRem, sigma=.05)
-plt.imshow(edgedetect, cmap=plt.cm.gray)
-locs=np.argwhere(edgedetect)
 
 #%%
 
 plt.plot(locs[:,1],locs[:,0],'.',markersize=1)
-plt.xlim(100,600)
+plt.xlim(0,300)
 plt.axes().set_aspect('equal')
 plt.tight_layout()
 
 #%%
-yminval=460
-ymaxval=690
 
-interval=np.logical_and(locs[:,0]>yminval,locs[:,0]<ymaxval)
-impDat=locs[interval]
-
-plt.plot(impDat[:,1],impDat[:,0],'.',markersize=2)
-
-splitline=np.mean(impDat[:,1][impDat[:,0]>ymaxval-10])
-
-plt.axvline(splitline)
-leadedge=np.max(impDat[:,1])
-trailedge=np.min(impDat[:,1])
-plt.axvline(leadedge)
-plt.axvline(trailedge)
 #%%
 leftdat=impDat[impDat[:,1]<splitline]
 plt.plot(leftdat[:,1],leftdat[:,0],'.',markersize=2)
