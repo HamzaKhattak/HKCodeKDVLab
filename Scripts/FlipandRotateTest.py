a6fd6897925c198802a65668f508d8f27ceb8817
Hamza Khattak <hamzakhattak@trentu,ca>
2019-07-11 16:39:21 -0400

    See above

--- Scripts/FlipandRotateTest.py
+++ Scripts/FlipandRotateTest.py
@@ -1,17 +1,16 @@
 import sys, os
 
-
+#Specify the location of the Tools folder
 CodeDR="F:\TrentDrive\Research\KDVLabCode\HKCodeKDVLab"
+#Specify where the data is and where plots will be saved
 dataDR="F:\TrentDrive\Research\Droplet forces film gradients\SlideData2"
 
 
-os.chdir(CodeDR)
-sys.path.append('./Tools')
-import DropletprofileFitter as df
-sys.path.remove('./Tools')
-print(os.getcwd())
+os.chdir(CodeDR) #Set  current working direcotry to the code directory
+sys.path.append('./Tools') #Add the tools to the system path so modules can be imported
+import DropletprofileFitter as df #Import required modules
+sys.path.remove('./Tools') #Remove tools from path
 
-
+#Set working directory to data location
 os.chdir(dataDR)
-
-print(df.flipper)
+#%%
