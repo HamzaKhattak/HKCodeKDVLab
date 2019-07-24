e815e3ec0a24887daa5ccfb42ae1144a7068adf1
HamzaKhattakLabPC <45341911+HamzaKhattak@users.noreply.github.com>
2019-07-24 18:04:00 -0400

    Update MultiImageAnalysis.py

--- Scripts/MultiImageAnalysis.py
+++ Scripts/MultiImageAnalysis.py
@@ -72,9 +72,7 @@ background=False
 filenames=glob.glob("*.tif")
 
 #Empty array for the position vs velocity information
-PosvtArray=[None]*len(filenames)
-AnglevtArray=[None]*len(filenames)
-EndptvtArray=[None]*len(filenames)
+dropProp=[None]*len(filenames)
 
 for i in range(len(filenames)):
     imagestack=ito.stackimport(dataDR + '\\' + filenames[i])
@@ -82,13 +80,44 @@ for i in range(len(filenames)):
     #Define no shift cropped image as first frame, could change easily if needed
     noshift=croppedimages[0]
     #Find the cross correlation xvt and save to position arrays
-    xvals,allcorr=crco.xvtfinder(croppedimages,noshift,cutpoint,guassfitl)
-    PosvtArray[i]=xvals[:,0]
+    xvals , allcorr=crco.xvtfinder(croppedimages,noshift,cutpoint,guassfitl)
+    PosvtArray = xvals[:,0]
     #Perform edge detection to get python array
-    stackedges=ede.seriesedgedetect(croppedimages,background,*imaparam)
+    stackedges = ede.seriesedgedetect(croppedimages,background,*imaparam)
     #Fit the edges and extract angles and positions
-    AnglevtArray[i], EndptvtArray[i] = df.edgestoproperties(stackedges,pixrange,fitfunc,fitguess)
-
+    AnglevtArray, EndptvtArray = df.edgestoproperties(stackedges,pixrange,fitfunc,fitguess)
+    #Reslice data to save for each file
+    dropProp[i]=np.vstack((PosvtArray,EndptvtArray.T,AnglevtArray.T)).T
+    #Save
+    fileLabel=os.path.splitext(filenames[i])
+    np.save(fileLabel[i]+'DropProps',dropProp[i])
+    
+    
 #%%
-plt.plot(EndptvtArray[0][:,0],label='left')
-plt.plot(EndptvtArray[0][:,1],label='right')
+gs = gridspec.GridSpec(3, 1)
+
+fig = plt.figure(figsize=(4,8))
+ax1 = fig.add_subplot(gs[0, 0])
+ax1.plot(dropProp[0][:,0],'b-',label=r'$1 \mu m /s$')
+ax1.plot(dropProp[1][:,0],'r-',label=r'$5 \mu m /s$')
+ax1.legend()
+ax1.set_ylabel('Pipette x (cc)')
+
+ax2 = fig.add_subplot(gs[1, 0]) 
+ax2.plot(dropProp[0][:,1],'b-')
+ax2.plot(dropProp[0][:,2],'b--')
+ax2.plot(dropProp[1][:,1],'r-')
+ax2.plot(dropProp[1][:,2],'r--')
+ax2.set_ylabel('Droplet x')
+ax2.legend()
+
+ax3 = fig.add_subplot(gs[2, 0]) 
+ax3.plot(dropProp[0][:,3],'b-')
+ax3.plot(-dropProp[0][:,4],'b--')
+ax3.plot(dropProp[1][:,3],'r-')
+ax3.plot(-dropProp[1][:,4],'r--')
+ax3.set_ylabel('Contact angle')
+ax3.set_xlabel('Time (s)')
+
+plt.tight_layout()
+
