1142edbbd5f45daeb3614f7db1b0c1e1aa2a72f9
HamzaKhattakLabPC <45341911+HamzaKhattak@users.noreply.github.com>
2019-08-08 12:22:19 -0400

    Remade the video conversion script

--- Scripts/ConvertToVid.ijm
+++
@@ -1,6 +0,0 @@
-args=split(getArgument(), ",")
-infilepath=args[0]
-outfilepath=args[1]
-open("infilepath");
-run("Movie...", "frame=100 container=.mp4 using=H.264 video=normal save=outfilepath");
-close();
--- Scripts/ImagetoVid.py
+++ Scripts/ImagetoVid.py
@@ -1,22 +1,43 @@
 '''
 This code is for quickly combining files for later analysis and then saving a video to make for easy playing
 '''
-import os
-dataDR="E:/Newtips/vidcombotest"
-inFile="1ums.tif"
-outFile="1umsv3.mov"
 
-VidConvArg=dataDR+inFile+","+dataDR+'outFile'
+import sys, os
+import importlib
+#need to install imageio and imagio-ffmpeg
+import imageio
+
+#%%
+#Specify the location of the Tools folder
+CodeDR=r"C:\Users\WORKSTATION\Desktop\HamzaCode\HKCodeKDVLab"
+#Specify where the data is and where plots will be saved
+dataDR=r"E:/Newtips/vidcombotest/"
+
+
+os.chdir(CodeDR) #Set  current working direcotry to the code directory
+
+
+sys.path.append('./Tools') #Add the tools to the system path so modules can be imported
+
+#Import required modules
+import ImportTools as ito 
+importlib.reload(ito)
+
+#Remove to avoid cluttering path
+sys.path.remove('./Tools') #Remove tools from path
+
+#Set working directory to data location
 os.chdir(dataDR)
-#Need to add fiji to path or similar for UNIX
-#Or use the trick of adding a .bit file in path that leads to it something like fiji.bat with:
-'''
-@echo off
-echo.
-C:\Users\WORKSTATION\PortPrograms\Fiji.app\ImageJ-win64.exe %*
-'''
 
-#Create video from tif stack using Fiji, could also maybe load into numpy array, but would likely be slower
-os.system("fiji --headless - macro ConvertToVid.ijm"+'\"'+VidConvArg+'\"')
-#Compressed video
-os.system("ffmpeg -i 1umsv3.mov -c:v libx264 -b:v 2M -maxrate 2M -bufsize 1M output.mp4 ")
+#%%
+inFile="1ums.tif"
+outFile="1umsv4.mp4"
+
+
+#Import the tif files in a folder
+imageframes=ito.folderstackimport(dataDR)
+#Or just one file
+#stackimport(dataDR+'/'+inFile)
+
+#Write to a video using mimwrite
+imageio.mimwrite(outFile, imageframes ,quality=5, input_params=['-r','100'],  output_params=['-r', '30'])
