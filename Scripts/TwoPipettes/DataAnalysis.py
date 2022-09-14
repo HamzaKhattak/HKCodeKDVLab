5e977736675b2246d2fcba9dc7136a883ba9d0d0
HamzaKhattak <45341911+HamzaKhattak@users.noreply.github.com>
2022-09-13 23:19:28 -0400

    Update DataAnalysis.py

--- Scripts/TwoPipettes/DataAnalysis.py
+++ Scripts/TwoPipettes/DataAnalysis.py
@@ -8,6 +8,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import savgol_filter
 import pynumdiff as pynd
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "sans-serif",
+    "font.sans-serif": ["Helvetica"]})
 #%%
 
 ex1dat = np.load('exp1.npy')
@@ -20,11 +24,14 @@ ex7dat = np.load('exp8.npy')
 #%%
 forward =[True,False,True,True,True,False,True]
 directionlist = [None]*len(forward)
+
+manual_angles = np.array([1.7,.8,.81,3.5,1.1,1.4,1])
+
 for i in range(len(forward)):
 	if forward[i]==False:
-		directionlist[i]='Backward'
+		directionlist[i]='B'
 	else:
-		directionlist[i] = 'Forward'
+		directionlist[i] = 'F'
 
 #%%
 
@@ -33,18 +40,22 @@ alldats = [ex1dat,ex2dat,ex3dat,ex4dat,ex5dat,ex6dat,ex7dat]
 meanangles = [np.mean(i[1]) for i in alldats]
 
 
-sorteddats = [x for _, x in sorted(zip(meanangles, alldats))]
-sortedlabels = [x for _, x in sorted(zip(meanangles, directionlist))]
-
+sorteddats = [x for _, x in sorted(zip(manual_angles, alldats))]
+sortedlabels = [x for _, x in sorted(zip(manual_angles, directionlist))]
+sortangles = np.sort(manual_angles)
+fig = plt.figure(figsize=(6,4))
 for i in range(len(sorteddats)):
 	med_angle = np.median(sorteddats[i][1])
+	med_angle = sortangles[i]
 	angle_sdev = np.std(sorteddats[i][1])
-	smoothed = savgol_filter(sorteddats[i][-1][20:-20],31,3)
-	derives = np.gradient(sorteddats[i][0][20:-20])
-	plt.plot(smoothed[50:-20],derives[50:-20],'.',label =rf'{med_angle*180/np.pi:.1f} $\pm$ {angle_sdev*180/np.pi:.1f}$\degree$ {sortedlabels[i]:s}')
+	smoothed = savgol_filter(sorteddats[i][2][20:-20],31,3)
+	derives = np.abs(np.gradient(sorteddats[i][0][20:-20]))
+	plt.plot(smoothed[50:-20]*2,derives[50:-20]*2,'.',label = "{0:.1f}$^\circ$ {1}".format(med_angle,sortedlabels[i]))
 plt.legend()
-plt.xlabel('distance from center')
-plt.ylabel('speed')
+plt.xlabel(r'$d \ (\mathrm{\mu m})$')
+plt.ylabel(r'$v (\mathrm{\mu m \ s^{-1}})$')
+plt.tight_layout()
+plt.savefig('multiangleplot.png',dpi=900)
 #%%
 
 #%%
