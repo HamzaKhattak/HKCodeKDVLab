687d7c7bc8a080ef64ee915a70761bfb6f2517b5
HamzaKhattak <45341911+HamzaKhattak@users.noreply.github.com>
2024-01-04 15:40:30 -0500

    Starting tracking
    Some initial work intro trackpy to get the locations of the droplets, may not be needed but could allow smoothing etc.

--- Scripts/FerroFluidTrack/dropfind.py
+++ Scripts/FerroFluidTrack/dropfind.py
@@ -19,6 +19,14 @@ import cv2 as cv
 from scipy.optimize import curve_fit
 
 from skimage.feature.peak import peak_local_max
+import pandas as pd
+from pandas import DataFrame, Series  # for convenience
+
+
+
+
+import pims
+import trackpy as tp
 
 def rescale(data):
 	'''
@@ -137,15 +145,7 @@ for i in range(100):
 #%%
 
 #%%
-import pandas as pd
-from pandas import DataFrame, Series  # for convenience
 
-
-
-
-import pims
-import trackpy as tp
-tp.link()
 #%%
 
 
@@ -171,7 +171,7 @@ def twoD_power2(xy, a, xo, yo, offset):
 def refinelocations(inputccor,initiallocs,windowsize):
 	#Create the meshgrid
 	locs = np.zeros([len(initiallocs),2])
-	x = np.linspace(0,2*windowsize-1,2*windowsize,dtype=int)
+	x = np.linspace(0,2*windowsize,2*windowsize+1,dtype=int)
 	y = x
 	X = np.meshgrid(x, y)
 
@@ -179,50 +179,159 @@ def refinelocations(inputccor,initiallocs,windowsize):
 	for i in range(len(initiallocs)):
 		yc = initiallocs[i,0]
 		xc = initiallocs[i,1]
-		cropped = inputccor[yc-windowsize:yc+windowsize , xc-windowsize:xc+windowsize]
+		cropped = inputccor[yc-windowsize:yc+windowsize+1 , xc-windowsize:xc+windowsize+1]
 		#initial_guess = (cropped[windowsize,windowsize],windowsize,windowsize-1,windowsize-1,windowsize,0,cropped[0,0])
-		initial_guess = (1,windowsize,windowsize,cropped[0,0])
+		initial_guess = (10,windowsize,windowsize,cropped[0,0])
 		inputdata = np.ravel(cropped)
-		bnds=((-np.inf, windowsize-2, windowsize-2, 0), (np.inf, windowsize+2, windowsize+2, 200))
-		popt, pcov = curve_fit(twoD_power2,X,inputdata,p0=initial_guess,maxfev=5000)
+		bnds=((-np.inf, windowsize-.5, windowsize-.5, -np.inf), (np.inf, windowsize+.5, windowsize+.5, np.inf))
+		popt, pcov = curve_fit(twoD_power2,X,inputdata,p0=initial_guess,maxfev=1000,bounds=bnds)
+		'''
+		if i ==400:
+			plt.imshow(cropped,cmap='gray')
+			plt.plot(windowsize,windowsize,'bo')
+			plt.plot(popt[1],popt[2],'ro')
+			print(popt)
+		'''
 		locs[i] = popt[1]+yc-windowsize,popt[2]+xc-windowsize
 	return locs
 
 testim,initiallocs,w1,h1 = findpositions(mainims[0],template1,mask1,.05,meth='cv.TM_CCOEFF_NORMED')
 initiallocs = initiallocs-[w1//2,h1//2]
-test = refinelocations(testim,initiallocs,3)
+test = refinelocations(testim,initiallocs,4)
 test = test+[w1//2,h1//2]
 initiallocs = initiallocs +[w1//2,h1//2]
-'''
+#%%
 plt.imshow(mainims[0],cmap='gray')
 plt.plot(test[:,1],test[:,0],'r.')
 plt.plot(initiallocs[:,1],initiallocs[:,0],'b.')
-'''
+
+
 #%%
-yc = initiallocs[80,0]
-xc = initiallocs[80,1]
+yc = initiallocs[5,0]+h1//2
+xc = initiallocs[5,1]+w1//2
 plt.imshow(testim[yc-3:yc+3 , xc-3:xc+3])
 #%%
-testnums = 10
+testnums = len(mainims)
+testnums=50
+alllocs = [None]*testnums
 allrefinedlocs = [None]*testnums
 for i in range(testnums):
-	print(i)
-	testim, w1, h1 = ccor(mainims[i],template1,mask1,meth='cv.TM_CCOEFF_NORMED')
-	testim,initiallocs,w1,h1 = findpositions(mainims[i],template1,mask1,.05,meth='cv.TM_CCOEFF_NORMED')
-	initiallocs = initiallocs-[w1//2,h1//2]
-	test = refinelocations(testim,initiallocs,3)
-	test = test+[w1//2,h1//2]
-	initiallocs = initiallocs +[w1//2,h1//2]
-	allrefinedlocs[i] = test
+	cor1,initiallocs1,w1,h1 = findpositions(mainims[i],template1,mask1,.05,meth='cv.TM_CCOEFF_NORMED')
+	cor2,initiallocs2,w2,h2 = findpositions(mainims[i],template2,mask2,.03,meth='cv.TM_CCOEFF_NORMED')
+	
+	initiallocs2 = removeduplicates(initiallocs1, initiallocs2, 8)
+	alllocs[i] = np.concatenate([initiallocs1,initiallocs2],axis=0)
+	
+	initiallocs1 = initiallocs1-[w1//2,h1//2]
+	refined1 = refinelocations(cor1,initiallocs1,4)
+	refined1 = refined1+[w1//2,h1//2]
+
+	initiallocs2 = initiallocs2-[w2//2,h2//2]
+	refined2 = refinelocations(cor2,initiallocs2,4)
+	refined2 = refined2+[w2//2,h2//2]	
+	
+	allrefinedlocs[i] = np.concatenate([refined1,refined2],axis=0)
+	if i%100==0:
+		print(i)
 
 
 #%%
 
+
+#%%
+import pickle
+def savelistnp(filepath,data):
+	'''
+	Saves lists of numpy arrays using pickle so they don't become objects
+	'''
+	with open(filepath, 'wb') as outfile:
+		   pickle.dump(data, outfile, pickle.HIGHEST_PROTOCOL)
+
+def openlistnp(filepath):
+	'''
+	Opens lists of numpy arrays using pickle
+	'''
+	with open(filepath, 'rb') as infile:
+	    result = pickle.load(infile)
+	return result
+savelistnp('refinedlocs.npy',allrefinedlocs)
+
+#%%
+frame = np.array([],dtype=float)
+x= np.array([],dtype=float)
+y=np.array([],dtype=float)
+#convert to dataframe:
+for i in range(len(allrefinedlocs)):
+	frame = np.append(frame, i*np.ones(len(allrefinedlocs[i])))
+	x = np.append(x,allrefinedlocs[i][:,0])
+	y = np.append(y,allrefinedlocs[i][:,1])
+
+
+pddat = pd.DataFrame({'frame': frame, 'x': x, 'y': y})
+
+
+#%%
+
+t1 = tp.link(pddat, 8,memory=30)
+
+
+t2 = tp.filter_stubs(t1,10)
+print('Before:', t1['particle'].nunique())
+print('After:', t2['particle'].nunique())
+d = tp.compute_drift(t2)
+
+tm = tp.subtract_drift(t2.copy(), d)
+tp.plot_traj(t2)
+ax = tp.plot_traj(tm)
+plt.show()
+#%%
+t4 = t2.loc[t2['particle'] == 15]
+print(len(t4))
+plt.plot(t4.x,t4.y,'.')
+#%%
+for i in np.arange(0,460,1):
+	ind = t2.loc[t2['particle'] == i]
+	print(i,':',len(ind))
+	if len(ind)<700:
+		print('test')
+		plt.plot(ind.y,ind.x)
+
+#%%
+indices= np.where(len(t2[['frame','x','y']].iloc[np.where(t2['particle']==i)]))
+
+#%%
+test = t2[['x', 'y','particle']].iloc[np.where(t2['frame']==0)]
+
+
+test2 = t2[['frame','x','y']].iloc[np.where(t2['particle']==100)]
+
+plt.plot(np.array(test2.x))
+
+plt.plot(test2.frame,test2.x)
+print(len(test2))
+
+#%%
+test3 = np.array(test2)
+#%%
+for i in range(t2['particle'].nunique()):
+	print(i,':',len(t2[['frame','x','y']].iloc[np.where(t2['particle']==i)]))
+
+
+#%%
+for i in range(len(mainims)):
+	print(len(allrefinedlocs[i]))
+	
+#%%
+testpositions = allrefinedlocs[]
+#%%
+test2 = t2[['frame','x','y']].iloc[np.where(t2['particle']==436)]
+print(len(test2))
 import matplotlib.animation as animation
-fig = plt.figure()
+fig,ax = plt.subplots()
 #line, = ax.plot([], [], lw=2)
-im=plt.imshow(mainims[0],cmap='gray')
-points, = plt.plot(allrefinedlocs[0][:,1],allrefinedlocs[0][:,0],'.')
+im=ax.imshow(mainims[0],cmap='gray')
+#points, = ax.plot(allrefinedlocs[0][:,1],allrefinedlocs[0][:,0],'.')
+points, = ax.plot(alllocs[0][:,1],alllocs[0][:,0],'.')
 # initialization function: plot the background of each frame
 # initialization function: plot the background of each frame
 def init():
@@ -233,12 +342,14 @@ def init():
 # animation function.  This is called sequentially
 def animate_func(i):
 	im.set_array(mainims[i])
-	points.set_data(allrefinedlocs[i][:,1],allrefinedlocs[i][:,0])
+	#points.set_data(allrefinedlocs[i][:,1],allrefinedlocs[i][:,0])
+	points.set_data(alllocs[i][:,1],alllocs[i][:,0])
+	#points.set_data(test2.y[i],test2.x[i])
 	return im,points,
 
 anim = animation.FuncAnimation(
                                fig, 
                                animate_func, 
-                               frames = 10,
+                               frames = len(test2),
                                interval = 1,blit=True, # in ms
                                )
