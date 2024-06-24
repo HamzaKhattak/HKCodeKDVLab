fee487c84e3dc875966d53d5f9e8031ed70de1dc
HamzaKhattak <45341911+HamzaKhattak@users.noreply.github.com>
2024-06-24 13:22:06 -0400

    Update Winder.ino

--- Hardware/Winder/Winder.ino
+++ Hardware/Winder/Winder.ino
@@ -1,18 +1,6 @@
 /*==========================================================================
- * This is a minimal sketch showing the usage of TeensyStep
- *  
- * STEP Pulses on Pin 2    (can be any pin)
- * DIR  Signall on Pin 3   (can be any pin)
- * 
- * The target position is set to 1000 steps relative to the
- * current position. The move command of the controller 
- * moves the motor to the target position.  
- *  
- * Default parameters are 
- * Speed:          800 steps/s
- * Acceleration:  2500 steps/s^2
- * 
- * (slow, but good to start with since they will work on any normal stepper)
+This sketch moves a feed motor back and forth and rotates a spindle motor to 
+allow for a wire to be coiled around a bobbin
  *
  ===========================================================================*/
 
@@ -22,46 +10,54 @@ Stepper spindlemotor(3, 4);       // STEP pin: 2, DIR pin: 3
 Stepper feedmotor(6,7);
 StepControl controller;    // Use default setting
 
-float mmperturn = 5.0;
-int feedmspr = 8000;
-distperstep = mmperturn/feedmspr;
-
-int spindlespr = 8000;
-
-float bobbinlength = .03 // in m
-float wirediameter = 0.5e-3
-
-turnsperbobbinL = int(bobbinlength/wirediameter)-1
-spindlestepsperlayer = turnsperbobbinL*
+float bobbinlength = 15 // length of the bobbin in mm
+float wirediameter = 0.5 // diameter of the wire in mm
 
 
 
-int m1speed = 60000;
-int m2speed = 15000;
-int timeacc = 1;
-int timetotal = 5;
+float leadpitch = 5.0; //mm per turn in the feed rail (ie pitch of lead screw)
+int feedspr = 8000; //steps per rotation in feed motor
+distperstep = mmperturn/feedmspr; //distance along bobbin per step of feed motor
+feedstepsplayer = int(bobbinlength * feedspr / leadpitch) // Number of steps to traverse bobbin
+
+
+int spindlespr = 8000; //steps per rotation in the spindle motor
+turnsperbobbinL = bobbinlength/wirediameter
+spindlestepsplayer = int(turnsperobbinL*spindlespr)
+
+
+float feedspeed = 1 // speed the feeder moves in mm/s
+feedsps = int(feedspeed * feedspr / leadpitch)// speed of feed motor in steps per second
+spindlesps = (feedspeed/wirediameter)*spindlespr // speed of the spindle motor in steps per second
+
+
+int timeacc = 3;
+
+
+
 void setup()
 {
-     motor1
-  .setAcceleration(m1speed/timeacc)
-  .setMaxSpeed(m1speed);
+     spindlemotor
+  .setAcceleration(spindlesps/timeacc)
+  .setMaxSpeed(spindlesps);
   
 
-   motor2
-  .setAcceleration(m2speed/timeacc)
-  .setMaxSpeed(m2speed);
+   feedmotor
+  .setAcceleration(feedsps/timeacc)
+  .setMaxSpeed(feedsps);
 
   delay(500);
 
-    for (int i = 0; i <= 1; i++) {
-    motor1.setTargetRel(m1speed*timetotal);  // Set target position to 1000 steps from current position
-    motor2.setTargetRel(m2speed*timetotal);  // Set target position to 1000 steps from current position
+    for (int i = 0; i <= 2; i++) {
+    spindlemotor.setTargetRel(spindlestepsplayer);  // Set target position 
+    feedmotor.setTargetRel(feedstepsplayer);  // Set target position 
 
-    controller.move(motor1,motor2);    // Do the move
+    controller.move(spindlemotor,feedmotor);    // Do the move
     
-    motor1.setTargetRel(m1speed*timetotal);  // Set target position to 1000 steps from current position
-    motor2.setTargetRel(m2speed*-1*timetotal);  // Set target position to 1000 steps from current position
+    spindlemotor.setTargetRel(spindlestepsplayer);  // Set target position
+    feedmotor.setTargetRel(-1*feedstepsplayer);  // Set target position
     controller.move(motor1,motor2);    // Do the move
+    controller.move(spindlemotor,feedmotor);
   }
 }
 
